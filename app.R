ffed07d75107d91ac3f6bd2f5ea7fa859638af94
DivadNojnarg <dgranjon@ymail.com>
2023-11-28 10:15:07 +0100

    refactor based on comments: try out admin mode

--- app.R
+++ app.R
@@ -4,32 +4,48 @@ library(pins)
 library(datamods)
 library(waiter)
 
-ui <- fluidPage(
-  theme = bslib::bs_theme(version = 5L),
-  tags$head(
-    tags$script(src = "app.js")
-  ),
-  useWaiter(),
-  actionButton("reset", "Reset data"),
-  edit_data_ui(id = "edit"),
-  validation_ui("validation", display = "inline")
+users <- data.frame(
+  name = c("Olajoke", "davidgranjon"),
+  is_admin = c(FALSE, FALSE)
 )
+
+whoami <- function() {
+  system("whoami", intern = TRUE)
+}
+
+ui <- function(request) {
+  fluidPage(
+    theme = bslib::bs_theme(version = 5L),
+    tags$head(
+      tags$script(src = "app.js")
+    ),
+    useWaiter(),
+    actionButton("reset", "Reset data"),
+    span(class = "badge bg-primary", "Admin:", textOutput("is_admin")),
+    edit_data_ui(id = "edit"),
+    validation_ui("validation", display = "inline")
+  )
+}
 
 server <- function(input, output, session) {
   board <- board_connect()
   w <- Waiter$new()
   dat <- pin_reactive_read(board, "user-input-poc-data", interval = 1000)
-
   cache <- reactiveValues(dat = NULL)
+
+  # Is user admin or not (will run once per user session)
+  is_admin <- users[users$name == whoami(), "is_admin"]
+
+  output$is_admin <- renderText(is_admin)
 
   observeEvent(input$reset, {
     board |> pin_write(
       cbind(
         iris,
         comment = rep("", nrow(iris)),
-        #last_updated_by = rep(NA, nrow(iris)),
-        locked_by = rep(NA, nrow(iris)),
-        locked = rep(FALSE, nrow(iris))),
+        last_updated_by = rep(NA, nrow(iris)),
+        locked = rep(FALSE, nrow(iris))
+      ),
       "user-input-poc-data"
     )
   })
@@ -46,36 +62,57 @@ server <- function(input, output, session) {
 
   }, ignoreNULL = FALSE, once = TRUE)
 
-  # Toggle all buttons according to the data state
+  # Toggle all buttons according to the data state but not for admins
   observeEvent(dat(), {
     # Create/update a cache which user can edit
     cache$dat <- dat()
-    session$sendCustomMessage("toggle-buttons", cache$dat$locked)
+    # Admin can edit all rows regardless of their locked state
+    locked <- if (is_admin) {
+      rep(FALSE, nrow(cache$dat))
+    } else {
+      # For a given user, we unlock all rows where she/he is the
+      # last editor so we can still provide corrections.
+      tmp <- which(cache$dat$last_updated_by == whoami())
+      dont_lock <- cache$dat$locked
+      if (length(tmp > 0)) {
+        dont_lock[tmp] <- FALSE
+      }
+      dont_lock
+    }
+    # Tell JS which button to lock/unlock
+    session$sendCustomMessage("toggle-buttons", locked)
   })
 
-  col_names <- reactive({
-    to_keep <- !(colnames(cache$dat) %in% c("locked", "locked_by"))
+  cols_to_show <- reactive({
+    to_keep <- !(colnames(cache$dat) %in% c("locked"))
     colnames(cache$dat)[to_keep]
+  })
+
+  cols_to_edit <- reactive({
+    to_edit <- !(colnames(cache$dat) %in% c("locked", "last_updated_by"))
+    colnames(cache$dat)[to_edit]
   })
 
   ## Note: the confirm button for edit can be accessed via <module_id>-update
   # LOCK button
   observeEvent(input[["edit-update"]], {
 
-    # If user accidentally closes modal without commiting data
+    # If user accidentally closes modal without committing data
     # we'll unlock the current row.
     session$sendCustomMessage("close-modal-callback", input[["edit-update"]])
 
-    # prevents from reloading the data within the session
+    # Pins is slow on connect so we must show a loader
     w$show()$update(
       html = tagList(
         p("Preparing the editor ..."),
         spin_flower()
       )
     )
+
+    # prevents from reloading the data within the session
     pin_data <- cache$dat
     pin_data[input[["edit-update"]], "locked"] <- TRUE
-    pin_data[input[["edit-update"]], "locked_by"] <- system("whoami", intern = TRUE)
+    pin_data[input[["edit-update"]], "last_updated_by"] <- whoami()
     board |> pin_write(pin_data, "user-input-poc-data")
     w$hide()
   })
@@ -96,14 +133,10 @@ server <- function(input, output, session) {
     ), {
     cache$dat <- cbind(
       res_edited(),
-      locked = dat()$locked,
-      locked_by = dat()$locked_by
+      locked = dat()$locked
     )
     # prevents from reloading the data within the session
     pin_data <- cache$dat
-    pin_data[input[["edit-update"]], "locked"] <- FALSE
-    pin_data[input[["edit-update"]], "locked_by"] <- NA
-    #pin_data[input[["edit-update"]], "last_updated_by"] <- system("whoami", intern = TRUE)
     board |> pin_write(pin_data, "user-input-poc-data")
   }, ignoreInit = TRUE)
 
@@ -111,28 +144,14 @@ server <- function(input, output, session) {
   res_edited <- edit_data_server(
     id = "edit",
     # Hide "locked" column to end users
-    data_r = reactive(cache$dat[, col_names()]),
+    data_r = reactive(cache$dat[, cols_to_show()]),
     add = FALSE,
     delete = FALSE,
     download_csv = FALSE,
     download_excel = FALSE,
-    var_edit = col_names(),
-    var_mandatory = col_names()
+    var_edit = cols_to_edit(),
+    var_mandatory = cols_to_edit()
   )
-
-  # Cleanup if crashed or when closed... Avoids
-  # to accidentally have locked rows by a given user.
-  session$onSessionEnded(function() {
-    # Only cleanup the current user rows ...
-    isolate({
-      to_update <- which(cache$dat$locked_by == system("whoami", intern = TRUE))
-      if (length(to_update) > 0) {
-        cache$dat$locked_by[to_update] <- rep(NA, length(to_update))
-        cache$dat$locked[to_update] <- rep(FALSE, length(to_update))
-        board |> pin_write(cache$dat, "user-input-poc-data")
-      }
-    })
-  })
 }
 
 shinyApp(ui, server)
