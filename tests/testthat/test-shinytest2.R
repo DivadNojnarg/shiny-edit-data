824d9e4ff99b1775a3bec87d4442c988fc77f664
DivadNojnarg <dgranjon@ymail.com>
2024-03-01 21:12:37 +0100

    screenshot delay

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -3,6 +3,5 @@ library(shinytest2)
 test_that("{shinytest2} recording: edit-data-poc", {
   app <- AppDriver$new(variant = platform_variant(), name = "edit-data-poc", height = 1294,
       width = 1525)
-  app$wait_for_idle()
-  app$expect_screenshot()
+  app$expect_screenshot(delay = 3)
 })
