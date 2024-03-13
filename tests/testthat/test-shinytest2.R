64ed26f894e5f9a54697da70fdeff9f50146939e
DivadNojnarg <dgranjon@ymail.com>
2024-03-13 22:29:35 +0100

    try fix test

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,4 +1,5 @@
 library(shinytest2)
+library(reactable)
 
 test_that("{shinytest2} recording: edit-data-poc", {
   app <- AppDriver$new(variant = platform_variant(), name = "edit-data-poc", height = 1294,
